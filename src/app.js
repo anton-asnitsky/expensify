76d8721b59b9e0e795c53284abd767d50218ba0d
antona <antona@walla.net.il>
2018-07-22 17:29:58 +0300

    Expensify boilerplate is ready

--- src/app.js
+++ src/app.js
@@ -3,3 +3,5 @@ import ReactDOM         from 'react-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
