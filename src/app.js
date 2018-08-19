ac888f663058b62e67cb27ec9101d8ae87abb1d3
antona <antona@walla.net.il>
2018-08-19 15:18:59 +0300

    Added tests fpr ExpenseForm comp

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import configureStore       from './store/configure-store';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 const jsx = (
